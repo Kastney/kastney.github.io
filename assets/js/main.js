091a4b9dc6b11d156857484c6a0e08e636bf5bde
Kastney <kastney@outlook.com>
2024-08-01 15:42:15 -0300

    ✨ feat - hiding the menu as the page scrolls

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 function getScrollY() {
-  var supportPageOffset = window.scrollX !== undefined;
-  var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");  
-  return supportPageOffset ? window.scrollY : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+  var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+  return window.scrollX !== undefined ? window.scrollY : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
 }
