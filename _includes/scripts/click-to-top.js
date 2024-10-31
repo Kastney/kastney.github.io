1a86a19e9434419f393ccbb8f464aaeb554a7338
Kastney <contato@kastney.com>
2024-10-30 23:50:31 -0300

    🐛 fix - Fixed click-to-top button behavior

--- _includes/scripts/click-to-top.js
+++ _includes/scripts/click-to-top.js
@@ -1,7 +1,13 @@
-function initClickToTap(){
+function initClickToTop() {
     var clickToTop = document.getElementById('click-to-top');
+    var header = document.querySelector('header');
+
     clickToTop.addEventListener('click', () => {
-        smoothScrollTo(window.scrollY >= 2 ? 1 : 0);
+        var hasBanner = header.getAttribute('banner') === 'true';
+        var scrollToPosition = hasBanner && window.scrollY >= 2 ? 1 : 0;
+        
+        smoothScrollTo(scrollToPosition);
     });
 }
-document.addEventListener('DOMContentLoaded', initClickToTap);
+
+document.addEventListener('DOMContentLoaded', initClickToTop);
