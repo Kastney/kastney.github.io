6c81de106c18c5597209d83e65da8dc1e6adf6e8
Kastney <contato@kastney.com>
2024-11-02 20:11:40 -0300

    ✨ feat - Create remove license file plugin

---
+++ _plugins/remove_license_file.rb
@@ -0,0 +1,7 @@
+Jekyll::Hooks.register :site, :post_write do |site|
+    license_file = File.join(site.dest, "LICENSE")
+    
+    if File.exist?(license_file)
+      File.delete(license_file)
+    end
+end  
